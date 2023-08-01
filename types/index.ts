6005bc2d2ce3b3e6a003e556c97d7fa4ebcaa695
Dev. JCLopez <dev.jclopez@gmail.com>
2023-08-01 11:15:15 -0500

    Home props added

--- types/index.ts
+++ types/index.ts
@@ -40,11 +40,15 @@ export interface CarProps {
 }
 
 export interface FilterProps {
-  manufacturer: string;
-  year: number;
-  fuel: string;
-  limit: number;
-  model: string;
+  manufacturer?: string;
+  year?: number;
+  fuel?: string;
+  limit?: number;
+  model?: string;
+}
+
+export interface HomeProps {
+  searchParams: FilterProps;
 }
 
 export interface ShowMoreProps {
